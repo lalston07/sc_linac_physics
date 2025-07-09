44079aa4a9b4f42dfbbc6bf414a31cf3160fc17c
lalston07 <leilaalston07@gmail.com>
2025-07-08 20:11:42 -0400

    Add files via upload

--- quench_waveform_pandas.py
+++ quench_waveform_pandas.py
@@ -4,7 +4,6 @@ import pandas as pd
 
 filename = 'ACCL_L3B_3180_20220630_164905_QUENCH.txt'
 timestamp = '2022-06-30_16:49:05.440831'
-time_timestamp = '2022-06-30_15:46:04.712966'
 
 # PV labels
 cavity_faultname = 'ACCL:L3B:3180:CAV:FLTAWF'
@@ -13,23 +12,34 @@ reverse_pow = 'ACCL:L3B:3180:REV:FLTAWF'
 decay_ref = 'ACCL:L3B:3180:DECAYREFWF'
 time_range = 'ACCL:L3B:3180:CAV:FLTTWF'
 
-def extracting_data(path_name, faultname, timestamp):
-    usecols = list(range(2, 2051))  
-    data_array = pd.read_csv(path_name, usecols=usecols, skiprows=51, nrows=1, header=None)
+def extracting_data(path_name, faultname):
+    with open(path_name, 'r') as file:
+        for line in file:
+            if faultname in line and f"{faultname}." not in line:
+                data = pd.Series(line.split())
+                target_timestamp = line.split()[1]
+                values = data[2:].astype(float).values  # extracts everything after timestamp
+                
+                print(f"{faultname} Information:")
+                print(f"Length of data: {len(values)}")
+                print(f"First value: {values[0]}, Last value: {values[-1]}")
+                print(f"Min value: {np.min(values)}, Max value: {np.max(values)}\n")
 
-    values = data_array.values[0]  # extracting the first row as a numpy array
+    return values, target_timestamp
 
-    print(f"{faultname} Information:")
-    print(f"Length: {len(values)}")
-    print(f"First: {values[0]}, Last: {values[-1]}")
-    print(f"Min: {np.min(values)}, Max: {np.max(values)}\n")
+# extracting all data for quench waveform
+cavity_data, cavity_time = extracting_data(filename, cavity_faultname)
+forward_data, forward_time = extracting_data(filename, forward_pow)
+reverse_data, reverse_time = extracting_data(filename, reverse_pow)
+decay_data, decay_time = extracting_data(filename, decay_ref)
+time_data, time_timestamp = extracting_data(filename, time_range)
 
-    return values
-
-cavity_data = extracting_data(filename, cavity_faultname, timestamp)
-
+# plotting data together
 plt.figure(figsize=(14,6))
-plt.plot(cavity_data, label="Cavity", color='blue', linewidth=2)
+plt.plot(time_data, cavity_data, label="Cavity", color='blue', linewidth=3)
+plt.plot(time_data, forward_data, label="Forward Power", color='green', linewidth=3)
+plt.plot(time_data, reverse_data, label="Reverse Power", color='red', linewidth=3)
+plt.plot(time_data, decay_data, label="Decay Reference", color='cyan', linewidth=1)
 plt.xlabel("Sample Index")
 plt.ylabel("Amplitude (MV)")
 plt.title("Cavity Quench Waveform")
