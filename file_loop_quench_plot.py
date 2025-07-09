bdeae93ad847f4775521292cab1d0a98b993e99a
lalston07 <leilaalston07@gmail.com>
2025-07-08 20:13:03 -0400

    Add files via upload

--- file_loop_quench_plot.py
+++ file_loop_quench_plot.py
@@ -5,6 +5,7 @@ import glob
 import os
 import re
 from datetime import datetime
+import pandas as pd
 
 # to extract data using a directory:
 directory_path = r"G:\My Drive\ACCL_L3B_3180"
@@ -19,12 +20,11 @@ directory_path = r"G:\My Drive\ACCL_L3B_3180"
 results = glob.glob(directory_path + '/**/*QUENCH.txt', recursive=True) # force this to have a number before _QUENCH
 print(f"Found {len(results)} matching QUENCH text files:")
 # print(results) # results is a list
-filtered_results = [f for f in results if re.search(r"\d+_QUENCH", f)]
-print(f"Found {len(filtered_results)} matching '##_QUENCH' text files:")
-print(filtered_results)
+quench_files = [f for f in results if re.search(r"\d+_QUENCH", f)]
+print(f"Found {len(quench_files)} matching '##_QUENCH' text files:")
+print(quench_files)
 
-
-for file in filtered_results: 
+for file in quench_files: 
     print("\nProcessing file: " + file)
     
     # getting PV and timestamp information from {file}
@@ -37,40 +37,38 @@ for file in filtered_results:
     # line below formats the timestamp to match the file layout
     timestamp = datetime.strptime(timestamp_raw, "%Y%m%d_%H%M%S").strftime("%Y-%m-%d_%H:%M:%S.")
     print("PV label: " + pv_base)
-    print("Timestamp: " + timestamp)
+    print("Timestamp: " + timestamp + "\n")
 
     # constructing PV label strings
     cavity_faultname = pv_base + ':CAV:FLTAWF'  # ex: ACCL:L3B:3180:CAV:FLTAWF
     forward_pow = pv_base + ':FWD:FLTAWF'       # ex: ACCL:L3B:3180:FWD:FLTAWF
     reverse_pow = pv_base + ':REV:FLTAWF'       # ex: ACCL:L3B:3180:REV:FLTAWF
     decay_ref = pv_base + ':DECAYREFWF'         # ex: ACCL:L3B:3180:DECAYREFWF    
-    time_range = pv_base + 'CAV:FLTTWF'         # ex: ACCL:L3B:3180:CAV:FLTTWF
+    time_range = pv_base + ':CAV:FLTTWF'         # ex: ACCL:L3B:3180:CAV:FLTTWF
 
-    # creating a function to extract the waveform data from each file
-    def extracting_data(file, faultname, timestamp): # change this so that we do not need the timestamp, we search for it later
-        section_lines = []
-        with open(file, 'r') as f:
-            for line in f:
-                if f"{faultname} {timestamp}" in line:
-                    section_lines.append(line)
-        
-        if not section_lines:
-           print("No matching lines found.")
-        else:
-            section_data = io.StringIO("".join(section_lines))
-            num_cols = len(section_lines[0].strip().split())
-            usecolumns = list(range(2, num_cols))
-            data_array = np.loadtxt(section_data, delimiter = None, usecols=usecolumns)
-            print(f"{faultname} data was extracted: first point is {data_array[0]}, and last point is {data_array[-1]}")
+    # creating a function to extract the waveform data and timestamps from each file
+    def extracting_data(path_name, faultname): 
+        with open(path_name, 'r') as file:
+            for line in file: 
+                if f"{faultname}" in line and f"{faultname}." not in line: 
+                    data = pd.Series(line.split())
+                    target_timestamp = line.split()[1]  # searching for timestamp in case it varies
+                    values = data[2:].astype(float).values
 
-            return data_array
+                    print(f"{faultname} Information:")
+                    print(f"Length of data: {len(values)}")
+                    print(f"First value: {values[0]}, Last value: {values[-1]}")
+                    print(f"Min value: {np.min(values)}, Max value: {np.max(values)}\n")
+
+                    return values, target_timestamp
+        return None, None   # added in case the PV line is not found
     
     # extract each waveform using defined function
-    cavity_data = extracting_data(file, cavity_faultname, timestamp)
-    forward_data = extracting_data(file, forward_pow, timestamp)
-    reverse_data = extracting_data(file, reverse_pow, timestamp)
-    decay_data = extracting_data(file, decay_ref, timestamp)
-    # time_data = extracting_data(file, time_range, timestamp)     # timestamp for time range is different
+    cavity_data, cavity_time = extracting_data(file, cavity_faultname)
+    forward_data, forward_time = extracting_data(file, forward_pow)
+    reverse_data, reverse_time = extracting_data(file, reverse_pow)
+    decay_data, decay_time = extracting_data(file, decay_ref)
+    time_data, time_timestamp = extracting_data(file, time_range)
 
 
 
